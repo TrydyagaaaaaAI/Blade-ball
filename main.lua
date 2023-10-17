fc94a23f14f7d684a32e4933e7034e2429e50367
TrydyagaaaaaAI <146543230+TrydyagaaaaaAI@users.noreply.github.com>
2023-10-17 21:17:08 +0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -9,13 +9,19 @@ local function createGUI()
     screenGui.Name = "AbilityChooser"
     screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
-    local mainMenuFrame = Instance.new("Frame")
-    mainMenuFrame.Size = UDim2.new(0.080, 100, 0.45, 100)
-    mainMenuFrame.Position = UDim2.new(0.15, -90, 0.4, -100)
-    mainMenuFrame.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2)
-    mainMenuFrame.BorderSizePixel = 0
-    mainMenuFrame.Name = "MainMenu"
-    mainMenuFrame.Parent = screenGui
+    local frame = Instance.new("Frame")
+    frame.Size = UDim2.new(0.080, 100, 0.45, 100)
+    frame.Position = UDim2.new(0.15, -90, 0.4, -100)
+    frame.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2)
+    frame.BorderSizePixel = 0
+    frame.Parent = screenGui
+
+    local minimizeButton = Instance.new("TextButton")
+    minimizeButton.Size = UDim2.new(0.1, 0, 0.05, 0)
+    minimizeButton.Position = UDim2.new(0.9, 0, 0, 0)
+    minimizeButton.BackgroundColor3 = Color3.fromRGB(37, 63, 96)
+    minimizeButton.Text = "-"
+    minimizeButton.Parent = frame
 
     local isDragging = false
     local dragInput
@@ -24,14 +30,14 @@ local function createGUI()
 
     local function update(input)
         local delta = input.Position - dragStart
-        mainMenuFrame.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+        frame.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
     end
 
-    mainMenuFrame.InputBegan:Connect(function(input)
+    frame.InputBegan:Connect(function(input)
         if input.UserInputType == Enum.UserInputType.Touch then
             isDragging = true
             dragStart = input.Position
-            startPos = mainMenuFrame.Position
+            startPos = frame.Position
 
             input.Changed:Connect(function()
                 if input.UserInputState == Enum.UserInputState.End then
@@ -41,7 +47,7 @@ local function createGUI()
         end
     end)
 
-    mainMenuFrame.InputChanged:Connect(function(input)
+    frame.InputChanged:Connect(function(input)
         if input.UserInputType == Enum.UserInputType.Touch then
             dragInput = input
         end
@@ -53,6 +59,42 @@ local function createGUI()
         end
     end)
 
+    minimizeButton.MouseButton1Click:Connect(function()
+        frame.Visible = not frame.Visible
+    end)
+
+    -- Ability function
+    local function Ability(abilityName)
+        ChosenAbility = abilityName
+        frame.Visible = false
+        local abilityMenuFrame = screenGui:FindFirstChild(abilityName)
+        if not abilityMenuFrame then
+            abilityMenuFrame = Instance.new("Frame")
+            abilityMenuFrame.Size = UDim2.new(0.4, 0, 0.4, 0)
+            abilityMenuFrame.Position = UDim2.new(0.3, 0, 0.3, 0)
+            abilityMenuFrame.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2)
+            abilityMenuFrame.BorderSizePixel = 0
+            abilityMenuFrame.Name = abilityName
+            abilityMenuFrame.Parent = screenGui
+
+            -- TODO: Add UI elements specific to the ability menu
+            -- Example: Add labels, buttons, etc.
+            local abilityButton = Instance.new("TextButton")
+            abilityButton.Size = UDim2.new(0.5, 0, 0.2, 0)
+            abilityButton.Position = UDim2.new(0.25, 0, 0.4, 0)
+            abilityButton.BackgroundColor3 = Color3.fromRGB(37, 63, 96)
+            abilityButton.TextColor3 = Color3.fromRGB(135, 206, 250)
+            abilityButton.Text = "Use Ability"
+            abilityButton.Parent = abilityMenuFrame
+
+            abilityButton.MouseButton1Click:Connect(function()
+                print("Ability used:", abilityName)
+            end)
+        else
+            abilityMenuFrame.Visible = true
+        end
+    end
+
     local abilities = {"Dash", "Forcefield", "Invisibility", "Platform", "Raging Deflection", "Shadow Step", "Super Jump", "Telekinesis", "Thunder Dash", "Freeze", "Infinity", "Pull", "Rapture", "Phase Bypass", "Waypoint"}
     local buttonHeight = 14
     for i, ability in ipairs(abilities) do
@@ -62,27 +104,10 @@ local function createGUI()
         button.Text = ability
         button.BackgroundColor3 = Color3.fromRGB(37, 63, 96)
         button.TextColor3 = Color3.fromRGB(135, 206, 250)
+        button.Parent = frame
 
-        button.Parent = mainMenuFrame
-        
         button.MouseButton1Click:Connect(function()
-            ChosenAbility = ability
-            mainMenuFrame.Visible = false
-            local abilityMenuFrame = screenGui:FindFirstChild(ability)
-            if not abilityMenuFrame then
-                abilityMenuFrame = Instance.new("Frame")
-                abilityMenuFrame.Size = UDim2.new(0.4, 0, 0.4, 0)
-                abilityMenuFrame.Position = UDim2.new(0.3, 0, 0.3, 0)
-                abilityMenuFrame.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2)
-                abilityMenuFrame.BorderSizePixel = 0
-                abilityMenuFrame.Name = ability
-                abilityMenuFrame.Parent = screenGui
-                
-                -- TODO: Add UI elements specific to the ability menu
-                -- Example: Add labels, buttons, etc.
-            else
-                abilityMenuFrame.Visible = true
-            end
+            Ability(ability)
         end)
     end
 end
@@ -93,17 +118,4 @@ local function onCharacterAdded(newCharacter)
     createGUI()
 end
 
-localPlayer.CharacterAdded:Connect(onCharacterAdded)
-createGUI()
-
-while task.wait() do
-    for _, obj in pairs(abilitiesFolder:GetChildren()) do
-        if obj:IsA("LocalScript") then
-            if obj.Name == ChosenAbility then
-                obj.Disabled = false
-            else
-                obj.Disabled = true
-            end
-        end
-    end
-end
+localPlayer.CharacterAdded:Connect(onCharacterAdded
