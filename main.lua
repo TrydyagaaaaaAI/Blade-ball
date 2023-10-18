171cfb4d03e7a3732b1fb4b72f7213a4675d9dcc
TrydyagaaaaaAI <146543230+TrydyagaaaaaAI@users.noreply.github.com>
2023-10-18 08:37:49 +0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,56 +1,116 @@
--- Создание кнопки-меню на экране
-local menuButton = Instance.new("TextButton")
-menuButton.Position = UDim2.new(0, 10, 0, 10)
-menuButton.Size = UDim2.new(0, 100, 0, 40)
-menuButton.Text = "Меню"
-menuButton.Parent = script.Parent -- Положение кнопки на экране
+local localPlayer = game.Players.LocalPlayer
+local character = localPlayer.Character or localPlayer.CharacterAdded:Wait()
+local abilitiesFolder = character:WaitForChild("Abilities")
 
--- При нажатии на кнопку-меню открывается окно с функциями
-menuButton.MouseButton1Click:Connect(function()
-    local menuFrame = Instance.new("Frame")
-    menuFrame.Position = UDim2.new(0, 10, 0, 60)
-    menuFrame.Size = UDim2.new(0, 200, 1, -70) -- Отступ по вертикали для кнопки-меню и нижней границы
-    menuFrame.BackgroundColor3 = Color3.new(1, 1, 1)
-    menuFrame.Parent = script.Parent -- Положение окна на экране
+local ChosenAbility = "Raging Deflection"
 
-    -- Функция main - отображение кнопок в окне
-    function main()
-        local function1Button = Instance.new("TextButton")
-        function1Button.Position = UDim2.new(0, 10, 0, 10)
-        function1Button.Size = UDim2.new(0, 180, 0, 30)
-        function1Button.Text = "Функция 1"
-        function1Button.Parent = menuFrame
+local function createGUI()
+    local screenGui = Instance.new("ScreenGui")
+    screenGui.Name = "AbilityChooser"
+    screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
-        local function2Button = Instance.new("TextButton")
-        function2Button.Position = UDim2.new(0, 10, 0, 50)
-        function2Button.Size = UDim2.new(0, 180, 0, 30)
-        function2Button.Text = "Функция 2"
-        function2Button.Parent = menuFrame
+    local frame = Instance.new("Frame")
+    frame.Size = UDim2.new(0.080, 100, 0.45, 100)
+    frame.Position = UDim2.new(0.15, -90, 0.4, -100)
+    frame.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2)
+    frame.BorderSizePixel = 0
+    frame.Parent = screenGui
 
-        local function3Button = Instance.new("TextButton")
-        function3Button.Position = UDim2.new(0, 10, 0, 90)
-        function3Button.Size = UDim2.new(0, 180, 0, 30)
-        function3Button.Text = "Функция 3"
-        function3Button.Parent = menuFrame
+    local isDragging = false
+    local dragInput
+    local dragStart
+    local startPos
 
-        -- Логика при нажатии на кнопки функций
-        function1Button.MouseButton1Click:Connect(function()
-            print("Вы выбрали функцию 1")
-            -- Здесь можно написать логику для выполнения функции 1
-        end)
-
-        function2Button.MouseButton1Click:Connect(function()
-            print("Вы выбрали функцию 2")
-            -- Здесь можно написать логику для выполнения функции 2
-        end)
-
-        function3Button.MouseButton1Click:Connect(function()
-            print("Вы выбрали функцию 3")
-            -- Здесь можно написать логику для выполнения функции 3
-        end)
+    local function update(input)
+        local delta = input.Position - dragStart
+        frame.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
     end
 
-    -- Запуск программы
-    print("Меню при запуске:")
-    main()
-end)
+    frame.InputBegan:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.Touch then
+            isDragging = true
+            dragStart = input.Position
+            startPos = frame.Position
+
+            input.Changed:Connect(function()
+                if input.UserInputState == Enum.UserInputState.End then
+                    isDragging = false
+                end
+            end)
+        end
+    end)
+
+    frame.InputChanged:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.Touch then
+            dragInput = input
+        end
+    end)
+
+    game:GetService("UserInputService").InputChanged:Connect(function(input)
+        if input == dragInput and isDragging then
+            update(input)
+        end
+    end)
+
+    local abilities = {"Dash", "Forcefield", "Invisibility", "Platform", "Raging Deflection", "Shadow Step", "Super Jump", "Telekinesis", "Thunder Dash", "Freeze", "Infinity", "Pull", "Rapture", "Phase Bypass", "Waypoint"}
+    local buttonHeight = 14
+    for i, ability in ipairs(abilities) do
+        local button = Instance.new("TextButton")
+        button.Size = UDim2.new(1, 0, 0, buttonHeight)
+        button.Position = UDim2.new(0, 0, 0, (i - 1) * (buttonHeight + 5))
+        button.Text = ability
+        button.BackgroundColor3 = Color3.new(0.8, 0.8, 0.8)
+        button.BorderColor3 = Color3.new(1, 1, 1)
+        button.Parent = frame
+        
+        button.MouseButton1Click:Connect(function()
+            ChosenAbility = ability
+        end)
+    end
+    
+    local collapseButton = Instance.new("TextButton")
+    collapseButton.Size = UDim2.new(1, 0, 0, 20)
+    collapseButton.Position = UDim2.new(0, 0, 1, -20)
+    collapseButton.Text = "Свернуть"
+    collapseButton.BackgroundColor3 = Color3.new(0.8, 0.8, 0.8)
+    collapseButton.BorderColor3 = Color3.new(1, 1, 1)
+    collapseButton.Parent = frame
+    
+    local isCollapsed = false
+
+    local function toggleCollapse()
+        isCollapsed = not isCollapsed
+        if isCollapsed then
+            collapseButton.Text = "Развернуть"
+            frame.Size = UDim2.new(0.080, 100, 0, 20)
+            frame.Position = UDim2.new(0.15, -90, 1, -20)
+        else
+            collapseButton.Text = "Свернуть"
+            frame.Size = UDim2.new(0.080, 100, 0.45, 100)
+            frame.Position = UDim2.new(0.15, -90, 0.4, -100)
+        end
+    end
+
+    collapseButton.MouseButton1Click:Connect(toggleCollapse)
+end
+
+local function onCharacterAdded(newCharacter)
+    character = newCharacter
+    abilitiesFolder = character:WaitForChild("Abilities")
+    createGUI()
+end
+
+localPlayer.CharacterAdded:Connect(onCharacterAdded)
+createGUI()
+
+while task.wait() do
+    for _, obj in pairs(abilitiesFolder:GetChildren()) do
+        if obj:IsA("LocalScript") then
+            if obj.Name == ChosenAbility then
+                obj.Disabled = false
+            else
+                obj.Disabled = true
+            end
+        end
+    end
+end
